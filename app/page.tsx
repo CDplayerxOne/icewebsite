a183b28059aae65c9773ddb6c7645eb5d47b6feb
Corey Dai <73034147+CDplayerxOne@users.noreply.github.com>
2024-06-01 12:00:50 -0400

    finished homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,154 @@
+"use client";
+
 import Image from "next/image";
+import bannerBackground from "../public/background.gif";
+import secondBanner from "../public/MainPageBackground2.jpg";
+import secondOverlay from "../public/MainPageLeft.jpg";
+import Education from "../public/Teacher.jpg";
+import Stock from "../public/Stock.jpg";
+
+import {
+  Parallax,
+  ParallaxBanner,
+  ParallaxBannerLayer,
+  ParallaxProvider,
+} from "react-scroll-parallax";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <ParallaxProvider>
+      <div className="w-full">
+        <div className="fixed w-full h-16 flex items-center z-50 bg-black">
+          <div className="basis-1/2">
+            <p className="text-blue-200 text-3xl ml-4 font-bold">AI Insights</p>
+          </div>
+          <div className="basis-1/2 flex justify-end mr-8">
+            <a href="#" className="text-blue-50 hover:text-blue-100 text-lg">
+              Economy
+            </a>
+            <a
+              href="#"
+              className="text-blue-50 hover:text-blue-100 text-lg ml-12"
+            >
+              Education
+            </a>
+          </div>
         </div>
+        <Parallax speed={-20}>
+          <ParallaxBanner className="aspect-video z-0">
+            <ParallaxBannerLayer speed={0} className="flex justify-center">
+              <div className="w-full">
+                <Image
+                  src={bannerBackground}
+                  className="w-full h-full max-w-none mt-8 brightness-50"
+                  alt="https://miro.medium.com/v2/resize:fit:1358/0*AU3rmB08Vntf33Is"
+                />
+              </div>
+            </ParallaxBannerLayer>
+            <ParallaxBannerLayer speed={-25}>
+              <div className="absolute inset-0 flex w-full items-center justify-center">
+                <h1 className="text-8xl text-white font-bold text-center">
+                  AI Insights: Exploring the Implications of AI
+                </h1>
+              </div>
+            </ParallaxBannerLayer>
+            <ParallaxBannerLayer speed={-25}></ParallaxBannerLayer>
+          </ParallaxBanner>
+        </Parallax>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <Parallax speed={30}>
+        <div className="overflow-hidden">
+          <div className="w-full h-screen absolute z-50 flex">
+            <div className="h-full w-1/2 flex justify-center items-center">
+              <div className="bg-black rounded-xl border-2 w-1/2 border-blue-100">
+                <h1 className="font-bold text-5xl text-blue-200 m-8">
+                  What is AI?
+                </h1>
+                <p className="text-white text-lg mx-8 mb-8">
+                  AI or artificial intelligence describes the ability for
+                  machines to replicate human-like intelligence. <br />
+                  <br />
+                  This form of intelligence is generally recognizable when a
+                  machine exhibits the ability to learn and adapt. <br /> <br />
+                  Generally, this capability requires the input of large amounts
+                  of data to be analyzed by a machine learning model.
+                </p>
+              </div>
+            </div>
+            <div className="h-full w-1/2 flex justify-center items-center overflow-hidden">
+              <Image
+                src={secondOverlay}
+                alt="firefly generated"
+                className="h-full brightness-90"
+              />
+            </div>
+          </div>
+          <Image
+            src={secondBanner}
+            className="h-screen overflow-hidden brightness-50"
+            alt="firefly generated"
+          />
+        </div>
+      </Parallax>
+      <div className="w-full">
+        <p className="text-white m-16 font-bold text-5xl">
+          As technology has advanced, so have the capabilities of AI. AI has now
+          become more widespread and widely used as ever. Its impact now spans a
+          wide array of industry sectors.{" "}
+          <span className="text-6xl text-blue-100">
+            <br />
+            <br />
+            This page is dedicated to exploring the impact of AI on our lives.{" "}
+          </span>
+        </p>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Parallax speed={30}>
+        <div className="h-screen w-full bg-blue-200 flex-col flex pt-16">
+          <div className="h-24 flex justify-center">
+            <h1 className="text-7xl font-bold m-8">Explore Sectors</h1>
+          </div>
+          <div className="grow flex items-center justify-center">
+            <div className="rounded-3xl w-2/5 h-5/6 bg-black mr-16 overflow-hidden relative shadow-xl">
+              <Image
+                src={Education}
+                alt="firefly generated teacher teaching students"
+                className="h-full"
+              />
+              <div className="absolute bottom-0 h-1/3 w-full bg-white z-50 flex flex-col">
+                <h2 className="font-bold text-3xl pt-8 px-8">Education</h2>
+                <p className="text-lg px-8">
+                  With the rise of generative AI such as ChatGPT, Gemini and
+                  other tools, the landscape in the classroom has completely
+                  changed.
+                </p>
+                <button className="text-white bg-blue-700 text-md rounded-xl w-32 p-2 m-4 ml-8 absolute bottom-0 hover:bg-blue-600">
+                  Learn More
+                </button>
+              </div>
+            </div>
+            <div className="rounded-3xl w-2/5 h-5/6 bg-black ml-16 overflow-hidden relative shadow-xl">
+              <Image
+                src={Stock}
+                alt="firefly generated teacher teaching students"
+                className="h-full"
+              />
+              <div className="absolute bottom-0 h-1/3 w-full bg-white z-50 flex flex-col">
+                <h2 className="font-bold text-3xl pt-8 px-8">Economy</h2>
+                <p className="text-lg px-8">
+                  The industry of producing AI is rapidly growing and is on
+                  course to accumulate a value increasing in the billions per
+                  year. The use of AI within companies and organizations has
+                  also revolutionized work.
+                </p>
+                <button className="text-white bg-blue-700 text-md w-32 rounded-xl p-2 m-4 ml-8 absolute bottom-0 hover:bg-blue-600">
+                  Learn More
+                </button>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </Parallax>
+    </ParallaxProvider>
   );
 }
